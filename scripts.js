cf6c6e53031e88c79c37e597407b143967d429b4
Kara Cavanaugh <karacav@msn.com>
2018-03-22 23:48:14 -0400

    solution 1

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// 1. Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+function deleteNth(arr, n) {
+  // ...
+  var result = [];
+  var itemCounts = {};
+  for (var i = 0; i < arr.length; i++) {
+    var item = arr[i];
+    var count = itemCounts[item] || 0;
+    if (count < n) {
+      result.push(item);
+      itemCounts[item] = count + 1;
+    }
+  }
+  return result;
+}
