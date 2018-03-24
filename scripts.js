a60c63709a6af43fcba731c5c68a30138bfd293c
Kara Cavanaugh <karacav@msn.com>
2018-03-24 10:38:30 -0400

    Solution 4

--- scripts.js
+++ scripts.js
@@ -55,3 +55,18 @@ function greetDevelopers(list) {
     })
   );
 }
+
+//Number 4: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Finish the solution so that it returns the SUM of all the multiples of 3 or 5 that are BELOW the number passed in.
+function solution(number) {
+  var myArr = [];
+  var totalNum = 0;
+  for (var i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      myArr.push(i);
+    }
+  }
+  for (var j = 0; j < myArr.length; j++) {
+    totalNum = totalNum + myArr[j];
+  }
+  return totalNum;
+}
