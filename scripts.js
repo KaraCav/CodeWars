2150ebb40d27c0a962d3653d1cb65ea120b22e2d
Kara Cavanaugh <karacav@msn.com>
2018-03-25 21:11:40 -0400

    Solution 6

--- scripts.js
+++ scripts.js
@@ -114,3 +114,9 @@ function countPositivesSumNegatives(input) {
     [0, 0]
   );
 }
+// Number 6: In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it. Code:  function withoutLast(arr) {
+//   arr.pop();
+//   return arr; }
+function withoutLast(arr) {
+  return arr.slice(0, -1);
+}
