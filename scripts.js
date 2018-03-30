66387d2afba8f9e4b9e52c9d5596de646cb067c5
Kara Cavanaugh <karacav@msn.com>
2018-03-30 17:19:36 -0400

    Solution 11

--- scripts.js
+++ scripts.js
@@ -172,3 +172,8 @@ var number = function(busStops) {
   }
   return totalPeeps;
 };
+
+// Number 11: Write a function that takes a string and return a new string with all vowels removed.
+function disemvowel(str) {
+  return str.replace(/[aeiou]+/gi, '');
+}
