74074cc605d56ffeb5537096461a3f21347aa28c
Kara Cavanaugh <karacav@msn.com>
2018-04-25 20:56:22 -0400

    Solution 2

--- CodewarsSql/Codewars1.sql
+++ CodewarsSql/Codewars1.sql
@@ -1,4 +1,6 @@
 -- NUMBER 1
 -- For this challenge you need to create a simple SELECT statement that will return all columns from the people table WHERE their age is over 50. You should return all people fields where their age is over 50 and order by the age descending.
 SELECT * FROM people WHERE age>50 ORDER BY age DESC;
-git remote set-url --add https://github.com/KaraCav/CodeWarsSQL.git
+
+--NUMBER 2: Fix the code below
+SELECT price * amount AS total FROM items
