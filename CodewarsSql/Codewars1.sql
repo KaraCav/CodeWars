f6304f3722e35a78811245a9bee4be414d4fda9a
Kara Cavanaugh <karacav@msn.com>
2018-04-26 21:32:08 -0400

    Solution 3

--- CodewarsSql/Codewars1.sql
+++ CodewarsSql/Codewars1.sql
@@ -4,3 +4,7 @@ SELECT * FROM people WHERE age>50 ORDER BY age DESC;
 
 --NUMBER 2: Fix the code below
 SELECT price * amount AS total FROM items
+-- NUMBER 3: Given the following table 'decimals':
+-- decimals table schema: id number1 number2
+-- Return a table with one column (mod) which is the output of number1 modulus number2.
+SELECT number1 * number2 AS mod FROM decimals 
