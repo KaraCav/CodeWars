02da1491cdaf269872789d3931c65949982c1c9b
Kara Cavanaugh <karacav@msn.com>
2018-05-06 21:53:59 -0400

    Solution 4

--- CodewarsSql/Codewars1.sql
+++ CodewarsSql/Codewars1.sql
@@ -4,7 +4,11 @@ SELECT * FROM people WHERE age>50 ORDER BY age DESC;
 
 --NUMBER 2: Fix the code below
 SELECT price * amount AS total FROM items;
+
 -- NUMBER 3: Given the following table 'decimals':
 -- decimals table schema: id number1 number2
 -- Return a table with one column (mod) which is the output of number1 modulus number2.
 select MOD(number1, number2) from decimals 
+
+--NUMBER 4: In your application, there is a section for adults only. You need to get a list of names and ages of users from the users table, who are 18 years old or older. Users table schema is just name and age.
+SELECT * FROM users WHERE age >= 18
